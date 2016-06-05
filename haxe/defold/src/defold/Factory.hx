f3f153043c02df1c803484eede3297d656216a7f
Dan Korostelev <nadako@gmail.com>
2016-06-05 22:24:31 +0300

    factory/collectionfactory externs

--- haxe/defold/src/defold/Factory.hx
+++ haxe/defold/src/defold/Factory.hx
@@ -4,5 +4,5 @@ import haxe.extern.EitherType;
 
 @:native("factory")
 extern class Factory {
-    static function create(url:EitherType<String,Url>, ?position:Vector3, ?rotation:Quaternion, ?properties:lua.Table.AnyTable, ?scale:EitherType<Float,Vector3>):Hash;
+    static function create(url:Url, ?position:Vector3, ?rotation:Quaternion, ?properties:lua.Table<Hash,lua.Table.AnyTable>, ?scale:EitherType<Float,Vector3>):Hash;
 }
