b6187fb5334a8de509c0d1da03174e2196b93290
Dan Korostelev <nadako@gmail.com>
2016-06-05 20:22:18 +0300

    add spine externs

--- haxe/defold/src/defold/Spine.hx
+++ haxe/defold/src/defold/Spine.hx
@@ -1,8 +1,14 @@
 package defold;
 
-import haxe.extern.EitherType;
+import defold.support.HashOrString;
 
 @:native("spine")
 extern class Spine {
-    static function play(url:EitherType<String,Url>, animation_id:EitherType<String,Hash>, playback:Playback, blend_duration:Float, ?complete_function:Void->Void):Void;
+    static function cancel(url:Url):Void;
+    static function get_go(url:Url, bone_id:HashOrString):Hash;
+    static function play(url:Url, animation_id:HashOrString, playback:Playback, blend_duration:Float, ?complete_function:Void->Void):Void;
+    static function reset_constant(url:Url, name:HashOrString):Void;
+    static function set_constant(url:Url, name:HashOrString, value:Vector4):Void;
+    static function set_ik_target(url:Url, ik_constraint_id:HashOrString, target_url:Url):Void;
+    static function set_ik_target_position(url:Url, ik_constraint_id:HashOrString, position:Vector3):Void;
 }
